c9780a8a5dba920cb920ccfaf47a4397ddaffbff
nabilazainina <nabilazainina@gmail.com>
2023-09-11 15:44:47 +0700

    Fixing Unit Test

--- main/tests.py
+++ main/tests.py
@@ -1,4 +1,6 @@
 from django.test import TestCase, Client
+from main.models import Items
+
 
 class mainTest(TestCase):
     def test_main_url_is_exist(self):
@@ -8,3 +10,13 @@ class mainTest(TestCase):
     def test_main_using_main_template(self):
         response = Client().get('/main/')
         self.assertTemplateUsed(response, 'main.html')
+
+    def test_show_main(self):
+        # Untuk mendapat respon dari main
+        response = Client().get('/main/')
+
+        # Mengecek konten dari response context
+        self.assertEqual(response.context['application_name'], 'glowventory')
+        self.assertEqual(response.context['name'], 'Bella')
+        self.assertEqual(response.context['class'], 'PBP C')
+
